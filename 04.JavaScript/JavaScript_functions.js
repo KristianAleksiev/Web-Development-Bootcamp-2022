339449b6204f9d4e83f4b310aa1d0542eb1ae43b
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-23 18:36:59 +0300

    Add files via upload

---
+++ 04.JavaScript/JavaScript_functions.js
@@ -0,0 +1,13 @@
+function doThis(){
+  // code block
+}
+doThis(); // and call it
+
+function getCoffee(){
+  alert("leaveHouse");
+  alert("moveRight");
+  alert("moveRight");
+  alert("goStraight");
+  alert("buyCoffee");
+}
+getCoffee();
