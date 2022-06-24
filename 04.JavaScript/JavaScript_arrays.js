52a3a06c43b3299c1492684c2de22f33d3cbe174
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-24 21:08:13 +0300

    Add files via upload

---
+++ 04.JavaScript/JavaScript_arrays.js
@@ -0,0 +1,8 @@
+var listNames = ["Kristian", "Mitko", "Georgi", "Petio", "Rosi"];
+
+var myName = listNames[0];
+
+listNames.length;
+listNames.includes("Rosi"); // Is the value in the array
+listNames.push("Emil"); // Appending an element in the end of the sequence
+listNames.pop(); // Remove the last item in the array in JS
