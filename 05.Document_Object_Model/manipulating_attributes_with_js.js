a81a64e521fbeebf35af2adb77eaec9d8c35bdcd
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-25 19:04:07 +0300

    Add files via upload

---
+++ 05.Document_Object_Model/manipulating_attributes_with_js.js
@@ -0,0 +1,7 @@
+href, src, everything in the element
+document.querySelector("a");
+document.querySelector("a").attributes;
+
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://www.amazon.com")
+// Attribute changed to another web adress
