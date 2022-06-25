bb0a57bdbe4b61c0ce6b2bbb012b31e3184c8804
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-25 19:04:43 +0300

    Add files via upload

---
+++ 05.Document_Object_Model/separation_of_concerns.js
@@ -0,0 +1,16 @@
+document.querySelector("button").classList; // - >
+document.quertSelector("button").classList.add("newCustomClass"); // We can add classes to an element
+//Styles in stylesheet, but manipulating it throu JS
+
+document.querySelector("button").classList.toggle("newCustomClass");
+
+
+
+// .huge{
+//   font-size: 10rem;
+//   color: red;
+//   font-weight: 500;
+// }
+
+document.quertSelector("h1").classList.add("huge");
+document.querySelector("h1").classList.toggle("huge");
