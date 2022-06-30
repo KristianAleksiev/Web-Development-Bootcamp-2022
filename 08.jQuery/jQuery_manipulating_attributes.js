4b6b7d11622c0c96f5ec696e61f4582cd9bce5d4
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-30 16:27:10 +0300

    Add files via upload

---
+++ 08.jQuery/jQuery_manipulating_attributes.js
@@ -0,0 +1,10 @@
+// src=, href=,..
+
+//Getting the value of an attribute
+$("img").attr("src");
+
+// Set the value of an attribute
+$("a").attr("href", "https://google.com");
+
+// Selecting a class attribute
+$("h1").attr("class"); // Getting all of the classes applied to h1
