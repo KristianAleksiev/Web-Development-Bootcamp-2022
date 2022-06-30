20a2a7ba66062541106a953bacef05d70564364f
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-30 16:28:07 +0300

    Add files via upload

---
+++ 08.jQuery/jQuery_manipulating_text.js
@@ -0,0 +1,8 @@
+//Changing the text of the element, in this case h1
+$("h1").text("New text");
+
+//Changing the text of all elements matching the criteria "button"
+$("button").text("Don't click me");
+
+// InnerHTML text manipulation with jQuery
+$("h1").html("<strong> New text bolded </strong>");
