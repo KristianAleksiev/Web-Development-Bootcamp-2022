685a3ed65abc1a56f2b311a1de4c243b8e6586be
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-30 16:26:14 +0300

    Add files via upload

---
+++ 08.jQuery/Incorporating_jQuery.js
@@ -0,0 +1,10 @@
+document.querySelector("h1") //vs
+$("h1")
+
+
+$("h1").css("color", "red");
+//Targetting the h1 element, changing its css color property to ..
+
+
+
+//jQuery relies on premade methods, ready to be used
