ba01f0162c81bcdfd1b2566afb8913f9339c6388
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-03 18:49:52 +0300

    Add files via upload

---
+++ 12.Application_Programming_Interfaces/API_endpoints_params_paths.js
@@ -0,0 +1,7 @@
+// API - set of commands, functions, protocols and objects that programmers use to interact with an external system(server)
+
+// Endpoint - base url
+// Paths - endpoint + params
+// Params - After ?, key value pair, separated by & - depending on the API, required params, optional params
+// JSON viewer pro
+// postman.com
