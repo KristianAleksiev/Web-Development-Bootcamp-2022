e1c215b95663ad974ea44d7b4a2d765042eb5192
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-05 19:45:56 +0300

    Add files via upload

---
+++ 14.Embedded_JavaScript/specific_scope.js
@@ -0,0 +1,13 @@
+if (true) {
+  var x = 2: // Global scope
+} else {
+  var y = 3; // Global scope
+}
+
+
+
+if (true) {
+  let t = 7; // Local scope - for/ while as well
+} else {
+  const p = 6; // Local scope
+}
