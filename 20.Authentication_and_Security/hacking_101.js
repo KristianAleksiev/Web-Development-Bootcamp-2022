d4e3e85af27a0f9b29cda71cf8d8751041ab18fa
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-13 20:54:04 +0300

    Add files via upload

---
+++ 20.Authentication_and_Security/hacking_101.js
@@ -0,0 +1,10 @@
+// Problem with hashing -> Forgotten passwords, Same passwords, Same hashes
+
+// How to hack email IN ORDER TO PROTECT THE USERS :))
+// Making a hash table
+// - All words from a dictionary
+// - All numbers from a phonebook
+// - All combinations of characters up to 6 places
+// - Total 20 billion combinations -> Nvidia RTX, 0.9 seconds
+
+// Time to brute force hack -> Longer passwords, exponentially longer time to crack
