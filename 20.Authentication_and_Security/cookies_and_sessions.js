8e42fe00313e6f32d744a963a011eba1e2c2a51a
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-13 20:54:43 +0300

    Add files via upload

---
+++ 20.Authentication_and_Security/cookies_and_sessions.js
@@ -0,0 +1,10 @@
+// Level 5 Security
+// Cookie ID number, Saving browsing sessions, Retargeting apps
+
+// Browser get request -> Amazon server
+// Amazon server response -> Browser
+// Browser POST request -> Amazon server
+// Amazon response (cookies +++) -> Browser
+
+// Session - Period of time where the browser interacts with a server
+// Cookies related to sessions activity
