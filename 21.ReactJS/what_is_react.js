118834bf1633b137c20ff226b4da11f045e85b74
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-14 19:35:23 +0300

    Add files via upload

---
+++ 21.ReactJS/what_is_react.js
@@ -0,0 +1,13 @@
+// Frontend framework -  library for building UIs
+// Creating repetitive elements
+// Component tree creation
+// Custom HTML alike
+// Combining html, css and js all together in each component
+// Whenever a change happens, compares the new version of the DOM with the old version, rerendering the change
+
+// Module content:
+// JSX, Props, Styling, Components, Hooks, ES6, Declarative programming, Babel,
+ // Spread operator, Destructuring, import and export, Map and filter, Virtual DOM, Container, State
+
+
+ // codesandbox.io
