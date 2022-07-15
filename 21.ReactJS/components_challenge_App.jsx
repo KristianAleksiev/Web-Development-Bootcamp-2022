cbc97a8ebc9b32fdbd68446b775ae1cc74fbfd41
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-15 21:58:27 +0300

    Add files via upload

---
+++ 21.ReactJS/components_challenge_App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Heading from "./components_challenge_Heading.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
+export default App;
